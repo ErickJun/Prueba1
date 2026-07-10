9d4eb713cef7a8768866caea30d853212b0b0547
ErickJun <135575007+ErickJun@users.noreply.github.com>
2026-07-10 12:40:22 -0500

    Update suma.js

--- suma.js
+++ suma.js
@@ -1,6 +1,10 @@
+import { numeros } from "./dato.js";
+ let total=0;
 
 
-export function suma(numeros){
-return numeros.reduce((total, num) => total + num,0);
+export const suma = (numeros) => {
+   for(let i=0;i<numeros.length;i++){
+      total+=numeros[i];
+   }
+   return total;
 }
-
